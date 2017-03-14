07678b683a77205f91b3c65e86c7a2659a65a435
Alex Shyman <alex.qqq33@gmail.com>
2017-03-14 20:03:36 +0300

    Первый коммит

---
+++ www/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://open/admin/');
+define('HTTP_CATALOG', 'http://open/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://open/admin/');
+define('HTTPS_CATALOG', 'http://open/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/open/www/admin/');
+define('DIR_SYSTEM', 'Z:/home/open/www/system/');
+define('DIR_IMAGE', 'Z:/home/open/www/image/');
+define('DIR_LANGUAGE', 'Z:/home/open/www/admin/language/');
+define('DIR_TEMPLATE', 'Z:/home/open/www/admin/view/template/');
+define('DIR_CONFIG', 'Z:/home/open/www/system/config/');
+define('DIR_CACHE', 'Z:/home/open/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/open/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/open/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/open/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/open/www/system/storage/upload/');
+define('DIR_CATALOG', 'Z:/home/open/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'open');
+define('DB_PASSWORD', 'open');
+define('DB_DATABASE', 'open');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ www/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://open/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://open/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/open/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/open/www/system/');
+define('DIR_IMAGE', 'Z:/home/open/www/image/');
+define('DIR_LANGUAGE', 'Z:/home/open/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/open/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/open/www/system/config/');
+define('DIR_CACHE', 'Z:/home/open/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/open/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/open/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/open/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/open/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'open');
+define('DB_PASSWORD', 'open');
+define('DB_DATABASE', 'open');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
